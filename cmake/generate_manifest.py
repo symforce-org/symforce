5729bc4f33c5f0da4b033b3a5e8b5f847b0a4d22
Aaron Miller <aaron@skydio.com>
2021-10-07 16:34:26 -0700

    [SymForce] Generate manifest from CMake for Make
    Generates a manifest.json with information that needs to get fed to
    makefiles later, like include and link directories, and the path to
    lcm-gen.  Currently I'm generating it into
    `symforce/build/manifest.json`.  The issue is that it needs to be
    somewhere that Python can find it - I'm not sure how to do this if CMake
    isn't being run by Python except to put it under the source directory.  I
    think unless someone has an obviously better idea, this is something we
    should revisit when we get to making SymForce pip installable, assuming
    it doesn't bite us before then.
    
    [Testing] Existing tests
    
    Topic: sym-manifest
    Reviewers: hayk, bradley

---
+++ cmake/generate_manifest.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+"""
+Generate a manifest.json file with build-related information known to CMake at configure time.
+
+Intended to be called from symforce/CMakeLists.txt.
+
+Examples are include paths of libraries we may want to build generated files against, as well as
+paths to symenginepy and the lcm-gen executable.  This json file is read by symforce/path_util.py,
+and Python users who need the information in the manifest should get it from there.
+"""
+
+import argparse
+import enum
+import json
+from pathlib import Path
+import re
+import typing as T
+
+
+class AllowedCount(enum.Enum):
+    SINGLE = enum.auto()
+    MULTIPLE = enum.auto()
+
+
+REQUIRED_KEYS = (
+    ("eigen_include_dirs", AllowedCount.MULTIPLE),
+    ("lcm_include_dirs", AllowedCount.MULTIPLE),
+    ("spdlog_include_dirs", AllowedCount.MULTIPLE),
+    ("catch2_include_dirs", AllowedCount.MULTIPLE),
+    ("lcm_gen_exe", AllowedCount.SINGLE),
+    ("symenginepy_install_dir", AllowedCount.SINGLE),
+)
+
+
+def parse_cmake_path_list(key, cmake_path_list):
+    # type: (str, str) -> T.List[str]
+    """
+    Take in a string representing a CMake list of paths, and parse a single path out of it.  If the
+    list contains multiple paths, it's required that all but one are generator expressions not
+    included in build targets.
+
+    Args:
+        cmake_path_list: A CMake list of paths
+
+    Returns:
+        The Python list of paths
+    """
+    # NOTE(aaron): Who knows what happens if someone puts cmake under a directory containing the ';'
+    # character? Or a directory that looks like a CMake generator expression? I don't
+    maybe_dirs = cmake_path_list.split(";")
+
+    paths = []
+
+    for maybe_dir in maybe_dirs:
+        if not maybe_dir:
+            # This one was the empty string
+            continue
+
+        maybe_generator_match = re.match(r"\$<([A-Za-z_]+):(.+)>", maybe_dir)
+        if maybe_generator_match is None:
+            # This one was non-empty and not a generator, assume it's a path
+            paths.append(maybe_dir)
+        else:
+            # This one is a generator
+            generator_type, path = maybe_generator_match.groups()
+
+            if generator_type == "BUILD_INTERFACE":
+                # This one is in the build interface, so keep it
+                paths.append(path)
+            else:
+                # This one was some other generator, skip it
+                continue
+
+    return paths
+
+
+def main():
+    # type: () -> None
+    parser = argparse.ArgumentParser()
+    for key, _ in REQUIRED_KEYS:
+        parser.add_argument("--{}".format(key), required=True)
+    parser.add_argument(
+        "--manifest_path", help="Where to put the generated manifest.json", required=True
+    )
+    args = parser.parse_args()
+
+    manifest = {}  # type: T.Dict[str, T.Union[str, T.List[str]]]
+    for key, allowed_count in REQUIRED_KEYS:
+        arg = getattr(args, key)
+        path_list = parse_cmake_path_list(key, arg)
+        if allowed_count == AllowedCount.SINGLE:
+            if len(path_list) != 1:
+                raise ValueError("Expected one path for argument {}, got: {}".format(key, arg))
+            manifest[key] = path_list[0]
+        elif allowed_count == AllowedCount.MULTIPLE:
+            if not path_list:
+                raise ValueError("Got no paths for {}: {}".format(key, arg))
+            manifest[key] = path_list
+
+    Path(args.manifest_path).parent.mkdir(parents=True, exist_ok=True)
+    with Path(args.manifest_path).open("w") as f:
+        json.dump(manifest, f)
+
+
+if __name__ == "__main__":
+    main()
--- symforce/path_util.py
+++ symforce/path_util.py
@@ -1,39 +1,63 @@
+import json
 from pathlib import Path
-
-***REMOVED*** = ***REMOVED***.parent
-
-
-def eigen_include_dir() -> Path:
-    return (***REMOVED*** / "include" / "eigen3").resolve()
+import typing as T
 
 
-def lcm_include_dir() -> Path:
-    return (***REMOVED*** / "include").resolve()
+class _Manifest:
+    """
+    Internal class to manage loading data from the build manifest and caching that data.  Not
+    intended for use outside of path_util.py.
+    """
+
+    _manifest = None
+
+    @classmethod
+    def _ensure_loaded(cls) -> None:
+        if cls._manifest is None:
+            with open(Path(__file__).parent.parent / "build" / "manifest.json") as f:
+                cls._manifest = json.load(f)
+
+    @classmethod
+    def get_entry(cls, key: str) -> Path:
+        cls._ensure_loaded()
+        assert cls._manifest is not None
+        return Path(cls._manifest[key]).resolve()
+
+    @classmethod
+    def get_entries(cls, key: str) -> T.List[Path]:
+        cls._ensure_loaded()
+        assert cls._manifest is not None
+        return [Path(s).resolve() for s in cls._manifest[key]]
 
 
-def spdlog_include_dir() -> Path:
-    return (***REMOVED*** / "_deps" / "spdlog-src" / "include").resolve()
+def eigen_include_dirs() -> T.List[Path]:
+    return _Manifest.get_entries("eigen_include_dirs")
+
+
+def lcm_include_dirs() -> T.List[Path]:
+    return _Manifest.get_entries("lcm_include_dirs")
+
+
+def spdlog_include_dirs() -> T.List[Path]:
+    return _Manifest.get_entries("spdlog_include_dirs")
 
 
 def symforce_dir() -> Path:
     return Path(__file__).parent.parent
 
 
-def catch2_include_dir() -> Path:
-    return symforce_dir() / "third_party" / "catch2"
-
-
-def fmt_library_dir() -> Path:
-    return (***REMOVED*** / "lib").resolve()
-
-
-def spdlog_library_dir() -> Path:
-    return (***REMOVED*** / "lib").resolve()
+def catch2_include_dirs() -> T.List[Path]:
+    return _Manifest.get_entries("catch2_include_dirs")
 
 
 def lcm_gen_exe() -> Path:
-    return (***REMOVED*** / "bin" / "lcm-gen").resolve()
+    return _Manifest.get_entry("lcm_gen_exe")
+
+
+def symenginepy_install_dir() -> Path:
+    return _Manifest.get_entry("symenginepy_install_dir")
 
 
 def skymarshal_exe() -> Path:
+    ***REMOVED*** = ***REMOVED***.parent
     return ***REMOVED*** / "bin" / "skymarshal"
