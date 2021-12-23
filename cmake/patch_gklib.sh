a8bbea89aceadf84a4483525b4485e9964091092
Aaron <aaron@skydio.com>
2021-12-23 16:23:01 -0700

    [SymForce] patch_gklib.sh works on macOS
    macOS sed is different from linux sed, this works on both
    
    See:
    https://stackoverflow.com/a/21243111
    https://stackoverflow.com/questions/37637777/what-does-h-command-mean-in-bash
    
    [Testing] Independent build succeeds on macOS
    
    Topic: sym-gklib-macos
    Reviewers: hayk,bradley
    GitOrigin-RevId: 698e63232420d5e12c17b66bcefdb3bfebdd811c

--- cmake/patch_gklib.sh
+++ cmake/patch_gklib.sh
@@ -8,8 +8,10 @@ gklib_src="$1"
 
 # GKlib adds -march=native on gcc, which is incorrect if cross-compiling.  This cmake file is
 # also included directly in METIS, so we can't just overwrite GKlib_COPTIONS, we have to patch.
-sed -i "${gklib_src}/GKlibSystem.cmake" -e "s/-march=native//"
+sed -i".bak" -e "s/-march=native//" "${gklib_src}/GKlibSystem.cmake"
+rm "${gklib_src}/GKlibSystem.cmake.bak"
 
 # They also add a "strings" executable in the test directory and have no option to not build
 # the test directory, so we need to disable that.
-sed -i "${gklib_src}/CMakeLists.txt" -e "s/add_subdirectory(\"test\")//"
+sed -i".bak" -e "s/add_subdirectory(\"test\")//" "${gklib_src}/CMakeLists.txt"
+rm "${gklib_src}/CMakeLists.txt.bak"
