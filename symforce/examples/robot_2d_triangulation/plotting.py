bec613ff407b88b728c8675623ab30befef71d7e
Hayk Martiros <hayk@skydio.com>
2022-01-16 15:27:53 -0800

    [SymForce] Add robot 2D triangulation example
    Demonstrates solving a 2D triangulation problem with SymForce. The goal is for a robot
    in a 2D plane to compute its trajectory given distance measurements from odometry
    and relative bearing angle measurements to known landmarks in the environment.
    
    Topic: symforce/tutorial_example
    GitOrigin-RevId: ba8ebcf0df7b11ed897d3852dd91025a49cca6e9

---
+++ symforce/examples/robot_2d_triangulation/plotting.py
@@ -0,0 +1,178 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.widgets import Slider
+import numpy as np
+
+import sym
+from symforce import typing as T
+from symforce.opt.optimizer import Optimizer
+from symforce.python_util import AttrDict
+from symforce.values import Values
+
+
+def plot_solution(optimizer: Optimizer, result: Optimizer.Result, animated: bool = False) -> None:
+    """
+    Visualize the optimization problem along its iterations. If animated is True, displays a
+    matplotlib animation instead of providing an interactive slider.
+    """
+    # Pull out values from the result
+    values_per_iter = [
+        optimizer.load_iteration_values(stats.values) for stats in result.iteration_stats
+    ]
+
+    # Create the layout
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_aspect("equal", adjustable="box")
+    plt.grid(True, alpha=0.5)
+    plt.tight_layout()
+
+    # Pull out quantities to plot
+    data = get_data_to_plot(result.optimized_values)
+
+    # Draw a circle at the origin
+    plt.scatter(x=[0], y=[0], color="black", s=50, zorder=2)
+
+    # Draw landmark locations
+    plt.scatter(data.landmark_xy[:, 0], data.landmark_xy[:, 1], color="orange", s=250, zorder=3)
+
+    # Draw poses
+    poses_circles = plt.scatter(
+        data.pose_xy[:, 0], data.pose_xy[:, 1], color="skyblue", zorder=3, s=500
+    )
+
+    # Draw lines connecting poses
+    poses_lines = plt.plot(
+        data.pose_xy[:, 0], data.pose_xy[:, 1], color="black", zorder=2, alpha=0.8
+    )
+
+    # Draw X/Y axes for pose locations
+    pose_vectors_x = plt.quiver(
+        data.pose_xy[:, 0],
+        data.pose_xy[:, 1],
+        data.pose_x_axes[:, 0],
+        data.pose_x_axes[:, 1],
+        zorder=4,
+        width=0.003,
+        color="blue",
+    )
+    pose_vectors_y = plt.quiver(
+        data.pose_xy[:, 0],
+        data.pose_xy[:, 1],
+        data.pose_y_axes[:, 0],
+        data.pose_y_axes[:, 1],
+        zorder=4,
+        width=0.003,
+        color="red",
+    )
+
+    # Draw dotted lines from poses to their landmark heading measurements
+    heading_arrows = [
+        plt.quiver(
+            data.pose_xy[:, 0],
+            data.pose_xy[:, 1],
+            data.heading_vectors[:, landmark_inx, 0],
+            data.heading_vectors[:, landmark_inx, 1],
+            scale=1.0,
+            zorder=2,
+            width=0.003,
+            linestyle=":",
+            facecolor="none",
+            linewidth=0.8,
+            alpha=0.5,
+            headwidth=0,
+            headlength=0,
+            capstyle="butt",
+        )
+        for landmark_inx in range(data.heading_vectors.shape[0])
+    ]
+
+    # Text box to write iteration stats
+    text = ax.text(3.0, -2.6, "-", fontsize=10)
+
+    def update_plot(slider_value: np.float64) -> None:
+        """
+        Update the plot using the given iteration.
+        """
+        num = int(slider_value)
+
+        # Set iteration text and abort if we rejected this iteration
+        stats = result.iteration_stats[num]
+        if num > 0 and not stats.update_accepted:
+            text.set_text(f"Iteration: {num} (rejected)\nError: {stats.new_error:.6f}")
+            return
+        text.set_text(f"Iteration: {num}\nError: {stats.new_error:.6f}")
+
+        # Get plottable data for this iteration
+        v = values_per_iter[num]
+        data = get_data_to_plot(v)
+
+        # Update the pose locations and connecting lines
+        poses_circles.set_offsets(data.pose_xy)
+        poses_lines[0].set_data(data.pose_xy.T)
+
+        # Update pose axes
+        pose_vectors_x.set_offsets(data.pose_xy)
+        pose_vectors_y.set_offsets(data.pose_xy)
+        pose_vectors_x.set_UVC(data.pose_x_axes[:, 0], data.pose_x_axes[:, 1])
+        pose_vectors_y.set_UVC(data.pose_y_axes[:, 0], data.pose_y_axes[:, 1])
+
+        # Update heading measurement vectors to landmarks
+        for landmark_inx in range(len(v["landmarks"])):
+            heading_arrows[landmark_inx].set_offsets(data.pose_xy)
+            heading_arrows[landmark_inx].set_UVC(
+                data.heading_vectors[:, landmark_inx, 0], data.heading_vectors[:, landmark_inx, 1]
+            )
+
+    if animated:
+        _ = animation.FuncAnimation(
+            fig, update_plot, len(values_per_iter), fargs=tuple(), interval=250
+        )
+        plt.show()
+    else:
+        # Add a slider fro iterations at the bottom of the plot
+        plt.subplots_adjust(bottom=0.2)
+        ax_slider = plt.axes([0.25, 0.1, 0.65, 0.03])
+        iteration_slider = Slider(
+            ax=ax_slider,
+            label="Iteration",
+            valmin=0,
+            valmax=len(values_per_iter) - 1,
+            valinit=len(values_per_iter) - 1,
+            valfmt="%0.0f",
+        )
+        iteration_slider.on_changed(update_plot)
+        iteration_slider.set_val(len(values_per_iter) - 1)
+        plt.show()
+
+
+def get_data_to_plot(v: Values) -> AttrDict:
+    """
+    Compute direct quantities needed for plotting.
+    """
+    data = AttrDict()
+
+    # Landmark positions
+    data.landmark_xy = np.array(v["landmarks"])
+
+    # Pose positions
+    data.pose_xy = np.array([p.position() for p in v["poses"]])
+
+    # Pose x/y axis vectors
+    data.pose_x_axes = np.array([p.rotation() * np.array([1, 0]) for p in v["poses"]])
+    data.pose_y_axes = np.array([p.rotation() * np.array([0, 1]) for p in v["poses"]])
+
+    # Measurement heading vectors from each pose to each landmark
+    data.heading_vectors = np.array(
+        [
+            [
+                v["poses"][i].rotation()
+                * sym.Rot2.from_tangent([np.deg2rad(v["angles"][i][landmark_inx])])
+                * np.array([50, 0])
+                for landmark_inx in range(len(v["landmarks"]))
+            ]
+            for i in range(len(v["poses"]))
+        ]
+    )
+
+    return data
---
+++ symforce/examples/robot_2d_triangulation/robot_2d_triangulation.py
@@ -0,0 +1,140 @@
+"""
+Demonstrates solving a 2D triangulation problem with SymForce. The goal is for a robot
+in a 2D plane to compute its trajectory given distance measurements from wheel odometry
+and relative bearing angle measurements to known landmarks in the environment.
+"""
+
+# -----------------------------------------------------------------------------
+# Define residual functions
+# -----------------------------------------------------------------------------
+from symforce import geo
+from symforce import sympy as sm
+from symforce import typing as T
+
+
+def bearing_residual(
+    pose: geo.Pose2, landmark: geo.V2, angle_deg: T.Scalar, epsilon: T.Scalar
+) -> geo.V1:
+    """
+    Residual from a relative bearing mesurement of a 2D pose to a landmark.
+    """
+    t_body = pose.inverse() * landmark
+    predicted_angle = sm.atan2_safe(t_body[1], t_body[0], epsilon=epsilon)
+    return geo.V1(sm.wrap_angle(predicted_angle - angle_deg * sm.pi / 180))
+
+
+def odometry_residual(
+    pose_a: geo.Pose2, pose_b: geo.Pose2, dist: T.Scalar, epsilon: T.Scalar
+) -> geo.V1:
+    """
+    Residual from the scalar distance between two poses.
+    """
+    return geo.V1((pose_b.t - pose_a.t).norm(epsilon=epsilon) - dist)
+
+
+# -----------------------------------------------------------------------------
+# Create a set of factors to represent the full problem
+# -----------------------------------------------------------------------------
+from symforce.opt.factor import Factor
+
+
+def build_factors(num_poses: int, num_landmarks: int) -> T.Iterator[Factor]:
+    """
+    Build factors for a problem of the given dimensionality.
+    """
+    for i in range(num_poses - 1):
+        yield Factor(
+            residual=odometry_residual,
+            keys=[f"poses[{i}]", f"poses[{i + 1}]", f"distances[{i}]", "epsilon"],
+        )
+
+    for i in range(num_poses):
+        for j in range(num_landmarks):
+            yield Factor(
+                residual=bearing_residual,
+                keys=[f"poses[{i}]", f"landmarks[{j}]", f"angles[{i}][{j}]", "epsilon"],
+            )
+
+
+# -----------------------------------------------------------------------------
+# Instantiate, optimize, and visualize
+# -----------------------------------------------------------------------------
+from symforce.opt.optimizer import Optimizer
+from symforce.values import Values
+
+
+def main() -> None:
+    # Create a problem setup and initial guess
+    num_poses = 3
+    num_landmarks = 3
+    initial_values = Values(
+        poses=[geo.Pose2.identity()] * num_poses,
+        landmarks=[geo.V2(-2, 2), geo.V2(1, -3), geo.V2(5, 2)],
+        distances=[1.7, 1.4],
+        angles=[[55, 245, -35], [95, 220, -20], [125, 220, -20]],
+        epsilon=sm.default_epsilon,
+    )
+
+    # Create factors
+    factors = build_factors(num_poses=num_poses, num_landmarks=num_landmarks)
+
+    # Select the keys to optimize - the rest will be held constant
+    optimized_keys = [f"poses[{i}]" for i in range(num_poses)]
+
+    # Create the optimizer
+    optimizer = Optimizer(
+        factors=factors,
+        optimized_keys=optimized_keys,
+        debug_stats=True,  # Return problem stats for every iteration
+        params=Optimizer.Params(verbose=True),  # Customize optimizer behavior
+    )
+
+    # Solve and return the result
+    result = optimizer.optimize(initial_values)
+
+    # Print some values
+    print(f"Num iterations: {len(result.iteration_stats) - 1}")
+    print(f"Final error: {result.error():.6f}")
+
+    for i, pose in enumerate(result.optimized_values["poses"]):
+        print(f"Pose {i}: t = {pose.position()}, heading = {pose.rotation().to_tangent()[0]}")
+
+    # Plot the result
+    # TODO(hayk): mypy gives the below error, but a relative import also doesn't work.
+    # Skipping analyzing "symforce.examples.robot_2d_triangulation.plotting":
+    #     found module but no type hints or library stubs
+    from symforce.examples.robot_2d_triangulation.plotting import plot_solution
+
+    plot_solution(optimizer, result)
+
+
+# -----------------------------------------------------------------------------
+# (Optional) Generate C++ functions for residuals with on-manifold jacobians
+# -----------------------------------------------------------------------------
+from pathlib import Path
+from symforce.codegen import Codegen, CppConfig
+
+
+def generate_bearing_residual_code() -> None:
+    """
+    Generate C++ code for the bearing residual function. A C++ Factor can then be
+    constructed and optimized from this function without any Python dependency.
+    """
+    # Create a Codegen object for the symbolic residual function, targeted at C++
+    codegen = Codegen.function(bearing_residual, config=CppConfig())
+
+    # Generate the function and print the code
+    metadata = codegen.generate_function()
+    print(open(metadata["generated_files"][0]).read())
+
+    # Create a Codegen object that computes jacobians from the residual Codegen object,
+    # by introspecting and symbolically differentiating the given arguments
+    codegen_with_jacobians = codegen.with_jacobians(which_args=["pose"])
+
+    # Generate the function and print the code
+    metadata = codegen_with_jacobians.generate_function()
+    print(open(metadata["generated_files"][0]).read())
+
+
+if __name__ == "__main__":
+    main()
