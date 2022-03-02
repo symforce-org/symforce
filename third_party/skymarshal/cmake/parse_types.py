1e28f015b08834db07e8045218d978d00ad708d1
Aaron <aaron@skydio.com>
2022-03-02 14:41:20 -0700

    [Skymarshal] Lint fixes
    Reviewers: eric,danny,will,abe,peter
    Topic: sym-ide-lint
    GitOrigin-RevId: 831e54983e4b2d9a3316741134316e2daafb9d0d

--- third_party/skymarshal/cmake/parse_types.py
+++ third_party/skymarshal/cmake/parse_types.py
@@ -1,11 +1,15 @@
+# aclint: py3
 import argh
+
 
 def main(lcmtypes_dir: str) -> None:
     import skymarshal.package_map
+
     package_map = skymarshal.package_map.parse_lcmtypes([lcmtypes_dir])
     for package, types in package_map.items():
         for type_name in types.type_definitions.keys():
             print(package, type_name)
+
 
 if __name__ == "__main__":
     argh.dispatch_command(main)
