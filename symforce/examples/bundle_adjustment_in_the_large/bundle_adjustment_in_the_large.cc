bad103554b50a26e56066dbbfcd8006162053954
Aaron <aaron@skydio.com>
2022-05-10 23:17:11 -0600

    [SymForce-External] Bundle-Adjustment-in-the-Large Example
    Import of https://github.com/symforce-org/symforce/pull/126
    
    ORIGINAL_AUTHOR = Aaron Miller <78561124+aaron-skydio@users.noreply.github.com>
    GitOrigin-RevId: 229df969c0b1ec34276897e468742105bffa817c

---
+++ symforce/examples/bundle_adjustment_in_the_large/bundle_adjustment_in_the_large.cc
@@ -0,0 +1,115 @@
+/* ----------------------------------------------------------------------------
+ * SymForce - Copyright 2022, Skydio, Inc.
+ * This source code is under the Apache 2.0 license found in the LICENSE file.
+ * ---------------------------------------------------------------------------- */
+
+#include <fstream>
+
+#include <sym/pose3.h>
+#include <symforce/opt/factor.h>
+#include <symforce/opt/internal/logging_configure.h>
+#include <symforce/opt/optimizer.h>
+#include <symforce/opt/values.h>
+
+#include "./gen/keys.h"
+#include "./gen/snavely_reprojection_factor.h"
+
+using namespace sym::Keys;
+
+sym::Factord MakeFactor(int camera, int point, int pixel) {
+  return sym::Factord::Hessian(sym::SnavelyReprojectionFactor<double>,
+                               {
+                                   sym::Key::WithSuper(CAM_T_WORLD, camera),
+                                   sym::Key::WithSuper(INTRINSICS, camera),
+                                   sym::Key::WithSuper(POINT, point),
+                                   sym::Key::WithSuper(PIXEL, pixel),
+                                   EPSILON,
+                               },
+                               /* optimized_keys = */
+                               {
+                                   sym::Key::WithSuper(CAM_T_WORLD, camera),
+                                   sym::Key::WithSuper(INTRINSICS, camera),
+                                   sym::Key::WithSuper(POINT, point),
+                               });
+}
+
+struct Problem {
+  std::vector<sym::Factord> factors;
+  sym::Valuesd values;
+  int num_cameras;
+  int num_points;
+  int num_observations;
+};
+
+/**
+ * Read the problem description from the given path
+ *
+ * See https://grail.cs.washington.edu/projects/bal/ for file format description
+ */
+Problem ReadProblem(const std::string& filename) {
+  std::ifstream file(filename);
+
+  int num_cameras, num_points, num_observations;
+  file >> num_cameras;
+  file >> num_points;
+  file >> num_observations;
+
+  std::vector<sym::Factord> factors;
+  sym::Valuesd values;
+
+  for (int i = 0; i < num_observations; i++) {
+    int camera, point;
+    file >> camera;
+    file >> point;
+
+    double px, py;
+    file >> px;
+    file >> py;
+
+    factors.push_back(MakeFactor(camera, point, i));
+    values.Set(sym::Key::WithSuper(PIXEL, i), Eigen::Vector2d(px, py));
+  }
+
+  for (int i = 0; i < num_cameras; i++) {
+    double rx, ry, rz, tx, ty, tz, f, k1, k2;
+    file >> rx;
+    file >> ry;
+    file >> rz;
+    file >> tx;
+    file >> ty;
+    file >> tz;
+    file >> f;
+    file >> k1;
+    file >> k2;
+
+    values.Set(sym::Key::WithSuper(CAM_T_WORLD, i),
+               sym::Pose3d(sym::Rot3d::FromTangent(Eigen::Vector3d(rx, ry, rz)),
+                           Eigen::Vector3d(tx, ty, tz)));
+    values.Set(sym::Key::WithSuper(INTRINSICS, i), Eigen::Vector3d(f, k1, k2));
+  }
+
+  for (int i = 0; i < num_points; i++) {
+    double x, y, z;
+    file >> x;
+    file >> y;
+    file >> z;
+
+    values.Set(sym::Key::WithSuper(POINT, i), Eigen::Vector3d(x, y, z));
+  }
+
+  values.Set(EPSILON, sym::kDefaultEpsilond);
+
+  return {std::move(factors), std::move(values), num_cameras, num_points, num_observations};
+}
+
+int main(int argc, char** argv) {
+  sym::internal::SetLogLevel("info");
+
+  SYM_ASSERT(argc == 2);
+  const auto problem = ReadProblem(argv[1]);
+
+  sym::Valuesd optimized_values = problem.values;
+  sym::Optimizerd optimizer{sym::DefaultOptimizerParams(), std::move(problem.factors)};
+  const auto stats = optimizer.Optimize(&optimized_values);
+  spdlog::info("Finished in {} iterations", stats.iterations.size());
+}
