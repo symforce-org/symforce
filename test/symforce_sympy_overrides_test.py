43c2dfd91f4b9d4264cca6d852fd542b7ca0b7ff
Aaron Miller <78561124+aaron-skydio@users.noreply.github.com>
2022-06-08 04:52:15 -0600

    [SymForce-External] [SymForce] Fix derivatives of sm.Mod
    Fixes symforce-org/symforce#163
    
    Also added unit tests for all our added derivatives
    
    Closes symforce-org/symforce#164
    
    GitOrigin-RevId: 22e39cc1d8251b93b624303402368cfb88b8eb76

--- test/symforce_sympy_overrides_test.py
+++ test/symforce_sympy_overrides_test.py
@@ -30,6 +30,37 @@ class SympyOverridesTest(TestCase):
         self.assertIsInstance(solution, T.List)
         self.assertEqual(set(solution), set())
 
+    def test_derivatives(self) -> None:
+        """
+        Tests:
+            sm.floor derivatives
+            sm.sign derivatives
+            sm.Mod derivatives
+        """
+        x, y = sm.symbols("x y")
+
+        self.assertEqual(sm.floor(x).diff(x), 0)
+        self.assertEqual(sm.floor(x ** 2).diff(x), 0)
+
+        self.assertEqual(sm.sign(x).diff(x), 0)
+        self.assertEqual(sm.sign(x ** 2).diff(x), 0)
+
+        def numerical_derivative(
+            f: T.Callable[[T.Scalar], T.Scalar], x: T.Scalar, delta: float = 1e-8
+        ) -> float:
+            return float((f(x + delta) - f(x - delta)) / (2 * delta))
+
+        for nx, ny in ((5, 2), (-5, 2), (5, -2), (-5, -2)):
+            self.assertAlmostEqual(
+                float(sm.Mod(x, y).diff(x).subs({x: nx, y: ny})),
+                numerical_derivative(lambda _x: sm.Mod(x, y).subs({x: _x, y: ny}), nx),
+            )
+
+            self.assertAlmostEqual(
+                float(sm.Mod(x, y).diff(y).subs({x: nx, y: ny})),
+                numerical_derivative(lambda _y: sm.Mod(x, y).subs({x: nx, y: _y}), ny),
+            )
+
 
 if __name__ == "__main__":
     TestCase.main()
