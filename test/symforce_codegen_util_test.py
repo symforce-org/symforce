7f7457974de70cc51f8ca072ba97899392804412
bradley-solliday-skydio <bradley.solliday@skydio.com>
2022-11-02 16:34:44 -0700

    Add codegen_util.load_generated_function
    Defines the new function load_generated_function which is
    meant to be a more user friendly version of `load_generated_package`
    (which will be particularly more friendly should we no longer re-export
    all sub-modules of generated packages).
    
    Also, explains the arguments of `load_generated_package` in its
    doc-string.
    
    As an example of the expected usage of this function, for `co` a
    `Codegen` object:
    ``` python3
    generated_paths = co.generate_function()
    
    func = load_generated_function(co.name, generated_paths.function_dir)
    ```
    
    Tested in `test/symforce_codegen_util_test.py`

--- test/symforce_codegen_util_test.py
+++ test/symforce_codegen_util_test.py
@@ -10,6 +10,10 @@ from symforce import typing as T
 from symforce.codegen import codegen_util
 from symforce.test_util import TestCase
 
+PKG_LOCATIONS = Path(__file__).parent / "test_data" / "codegen_util_test_data"
+RELATIVE_PATH = Path("example_pkg", "__init__.py")
+PACKAGE_NAME = "example_pkg"
+
 
 class SymforceCodegenUtilTest(TestCase):
     """
@@ -22,14 +26,8 @@ class SymforceCodegenUtilTest(TestCase):
             codegen_util.load_generated_package
         """
 
-        pkg_locations = Path(__file__).parent / "test_data" / "codegen_util_test_data"
-
-        relative_path = Path("example_pkg", "__init__.py")
-
-        package_name = "example_pkg"
-
         pkg1 = codegen_util.load_generated_package(
-            name=package_name, path=pkg_locations / "example_pkg_1" / relative_path
+            name=PACKAGE_NAME, path=PKG_LOCATIONS / "example_pkg_1" / RELATIVE_PATH
         )
 
         # Testing that the module was loaded correctly
@@ -37,16 +35,40 @@ class SymforceCodegenUtilTest(TestCase):
         self.assertEqual(pkg1.sub_module.sub_module_id, 1)
 
         # Testing that sys.modules was not polluted
-        self.assertFalse(package_name in sys.modules)
+        self.assertFalse(PACKAGE_NAME in sys.modules)
 
         pkg2 = codegen_util.load_generated_package(
-            name=package_name, path=pkg_locations / "example_pkg_2" / relative_path
+            name=PACKAGE_NAME, path=PKG_LOCATIONS / "example_pkg_2" / RELATIVE_PATH
         )
 
         # Testing that the module was loaded correctly when a module with the same name has
         # already been loaded
         self.assertEqual(pkg2.package_id, 2)
         self.assertEqual(pkg2.sub_module.sub_module_id, 2)
+
+    def test_load_generated_function(self) -> None:
+        """
+        Tests:
+            codegen_util.load_generated_function
+        """
+
+        func1 = codegen_util.load_generated_function(
+            func_name="func", path_to_package=PKG_LOCATIONS / "example_pkg_1" / RELATIVE_PATH
+        )
+
+        # Testing that the function was loaded correctly
+        self.assertEqual(func1(), 1)
+
+        # Testing that sys.modules was not polluted
+        self.assertFalse(PACKAGE_NAME in sys.modules)
+
+        func2 = codegen_util.load_generated_function(
+            func_name="func", path_to_package=PKG_LOCATIONS / "example_pkg_2" / RELATIVE_PATH
+        )
+
+        # Testing that the function was loaded correctly when a function of the same name
+        # has already been loaded.
+        self.assertEqual(func2(), 2)
 
 
 if __name__ == "__main__":
