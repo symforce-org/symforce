b16b2cb35e395338fb516ec8dbc077601ff7ff65
Bradley Solliday <84410250+bradley-solliday-skydio@users.noreply.github.com>
2022-11-04 00:10:43 +0000

    [SymForce-External] Add codegen_util.load_generated_function
    Add codegen_util.load_generated_function
    
    Defines the new function `load_generated_function` which is meant to be a more user friendly version of `load_generated_package` (which will be particularly more friendly should we no longer re-export all sub-modules of generated packages).
    
    Also, explains the arguments of `load_generated_package` in its doc-string.
    
    As an example of the expected usage of this function, for `co` a `Codegen` object:
    ``` python3
    generated_paths = co.generate_function()
    
    func = load_generated_function(co.name, generated_paths.function_dir)
    ```
    
    Tested in `test/symforce_codegen_util_test.py`
    Closes symforce-org/symforce#259
    
    GitOrigin-RevId: 75dd987bb893acabdf9fe0f3d904c7b362a1f0b3

---
+++ test/test_data/codegen_util_test_data/example_pkg_1/example_pkg/func.py
@@ -0,0 +1,8 @@
+# ----------------------------------------------------------------------------
+# SymForce - Copyright 2022, Skydio, Inc.
+# This source code is under the Apache 2.0 license found in the LICENSE file.
+# ----------------------------------------------------------------------------
+
+
+def func() -> int:
+    return 1
