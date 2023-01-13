fc4b4d5d549c9390c021724042140d261715ac96
Aaron <aaron@skydio.com>
2023-01-13 22:49:39 +0000

    [SymForce] More logic methods
    Added:
    - sf.less_equal
    - sf.greater_equal
    - sf.less
    - sf.greater
    
    Picked these names to match [NumPy's](https://numpy.org/doc/stable/reference/routines.logic.html), which also match aliases included by [PyTorch](https://pytorch.org/docs/stable/torch.html#comparison-ops) (presumably also to match NumPy).
    
    sf.logical_and and sf.logical_or also now take more than 2 arguments
    
    Also added unit tests for all this
    
    Reviewers: bradley,nathan,chao,hayk
    Topic: sf-logic
    Relative: sf-vector-access
    GitOrigin-RevId: 65e53273cce30d6a0db6e27d9967574d6e430ef7

--- test/symforce_logic_test.py
+++ test/symforce_logic_test.py
@@ -8,9 +8,9 @@ from symforce import typing as T
 from symforce.test_util import TestCase
 
 
-class SympyLogicMethodsTest(TestCase):
+class SymforceLogicTest(TestCase):
     """
-    Test logic methods added to the sympy module
+    Test logic methods
     """
 
     def test_is_positive(self) -> None:
@@ -49,6 +49,42 @@ class SympyLogicMethodsTest(TestCase):
         self.assertEqual(sf.is_nonpositive(0.0).evalf(), 1.0)
         self.assertEqual(sf.is_nonpositive(-0.1).evalf(), 1.0)
 
+    def test_less_equal(self) -> None:
+        """
+        Tests:
+            sf.less_equal
+        """
+        self.assertEqual(sf.less_equal(0.0, 1.0).evalf(), 1.0)
+        self.assertEqual(sf.less_equal(0.0, 0.0).evalf(), 1.0)
+        self.assertEqual(sf.less_equal(1.0, 0.0).evalf(), 0.0)
+
+    def test_greater_equal(self) -> None:
+        """
+        Tests:
+            sf.greater_equal
+        """
+        self.assertEqual(sf.greater_equal(0.0, 1.0).evalf(), 0.0)
+        self.assertEqual(sf.greater_equal(0.0, 0.0).evalf(), 1.0)
+        self.assertEqual(sf.greater_equal(1.0, 0.0).evalf(), 1.0)
+
+    def test_less(self) -> None:
+        """
+        Tests:
+            sf.less
+        """
+        self.assertEqual(sf.less(0.0, 1.0).evalf(), 1.0)
+        self.assertEqual(sf.less(0.0, 0.0).evalf(), 0.0)
+        self.assertEqual(sf.less(1.0, 0.0).evalf(), 0.0)
+
+    def test_greater(self) -> None:
+        """
+        Tests:
+            sf.greater
+        """
+        self.assertEqual(sf.greater(0.0, 1.0).evalf(), 0.0)
+        self.assertEqual(sf.greater(0.0, 0.0).evalf(), 0.0)
+        self.assertEqual(sf.greater(1.0, 0.0).evalf(), 1.0)
+
     SCALARS_TO_CHECK = (-2, -1, -0.9, -0.1, 0.0, 0.1, 0.9, 1, 2)
     UNSAFE_SCALARS_TO_CHECK = (0.0, 1.0)
 
@@ -63,10 +99,22 @@ class SympyLogicMethodsTest(TestCase):
     def check_logical_and(self, scalars: T.Sequence[float], unsafe: bool) -> None:
         for a in scalars:
             for b in scalars:
-                expected = self.bool_to_sym_bool(
-                    self.sym_bool_to_bool(a) and self.sym_bool_to_bool(b)
-                )
-                self.assertEqual(float(sf.logical_and(a, b, unsafe)), float(expected))
+                with self.subTest(a=a, b=b, unsafe=unsafe):
+                    expected = self.bool_to_sym_bool(
+                        self.sym_bool_to_bool(a) and self.sym_bool_to_bool(b)
+                    )
+                    self.assertEqual(float(sf.logical_and(a, b, unsafe=unsafe)), float(expected))
+
+                for c in scalars:
+                    with self.subTest(a=a, b=b, c=c, unsafe=unsafe):
+                        expected = self.bool_to_sym_bool(
+                            self.sym_bool_to_bool(a)
+                            and self.sym_bool_to_bool(b)
+                            and self.sym_bool_to_bool(c)
+                        )
+                        self.assertEqual(
+                            float(sf.logical_and(a, b, c, unsafe=unsafe)), float(expected)
+                        )
 
     def test_logical_and(self) -> None:
         """
@@ -79,10 +127,22 @@ class SympyLogicMethodsTest(TestCase):
     def check_logical_or(self, scalars: T.Sequence[float], unsafe: bool) -> None:
         for a in scalars:
             for b in scalars:
-                expected = self.bool_to_sym_bool(
-                    self.sym_bool_to_bool(a) or self.sym_bool_to_bool(b)
-                )
-                self.assertEqual(float(sf.logical_or(a, b, unsafe)), float(expected))
+                with self.subTest(a=a, b=b, unsafe=unsafe):
+                    expected = self.bool_to_sym_bool(
+                        self.sym_bool_to_bool(a) or self.sym_bool_to_bool(b)
+                    )
+                    self.assertEqual(float(sf.logical_or(a, b, unsafe=unsafe)), float(expected))
+
+                for c in scalars:
+                    with self.subTest(a=a, b=b, c=c, unsafe=unsafe):
+                        expected = self.bool_to_sym_bool(
+                            self.sym_bool_to_bool(a)
+                            or self.sym_bool_to_bool(b)
+                            or self.sym_bool_to_bool(c)
+                        )
+                        self.assertEqual(
+                            float(sf.logical_or(a, b, c, unsafe=unsafe)), float(expected)
+                        )
 
     def test_logical_or(self) -> None:
         """
