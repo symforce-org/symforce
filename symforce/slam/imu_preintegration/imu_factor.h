ab1c62116c35deddfb99061261397fbed0ec88c6
Bradley Solliday <84410250+bradley-solliday-skydio@users.noreply.github.com>
2023-02-03 05:07:07 +0000

    [SymForce-External] [ImuFactor] Add an on manifold Imu Factor
    The core use-facing api is found in
    - symforce/slam/imu_preintegration/preintegrated_imu_measurements.h
    - symforce/slam/imu_preintegration/imu_factor.h
    
    Unlike most other factors (/residual functions) in symforce, the
    ImuFactor is a functor so as to store the pre-integrated values
    with the function rather than in the `Values`. (There are a lot of
    parameters that are constant and unique to each factor).
    
    [ImuFactor] Unit test the on manifold ImuFactor
    
    Adds four tests meant to jointly test the
    `ImuManifoldPreintegrationUpdate` function.
    
    The calculated covariance is tested by sampling the values it is a
    covariance of and comparing it to the numircally calculated covariance
    from the samples. Warning, there is a tradeoff between time (number of
    samples) and accuracy for the calculated covariance. To speed things up,
    the sampling process is multi-threaded. Note, with my current parameters,
    it takes about 2^23 samples to be pretty robust to changes in the random
    seed. But that takes a minute to run on my desktop with 16 cores. So in
    the interest of testing time, I've cut the sample size by a bit. Of
    course, this means if you change the seed, the test might end up
    failing. To know if it's really a problem, bump up the sample size and
    see if it persists.
    
    The derivatives wrt to bias are calculated by perturbing the input in
    each direction to numerically calculate the columns of the derivative.
    
    The actual change in state (DR, Dv, Dp) isn't tested in a very
    intelligent manner. They didn't have any simple and obvious properties
    that could be easily tested. Still, their implementations are pretty
    simple, so I figured comparing to a C++ implementation would at the very
    least protect against someone accidentally deleting a line in the
    symbolic code or whatever.
    
    Topic: on_manifold_imu_factor_test
    Relative: on_manifold_imu_factor
    Closes https://github.com/symforce-org/symforce/pull/279
    Closes https://github.com/symforce-org/symforce/pull/280
    GitOrigin-RevId: 31bcaf8dbcf7b967f03199fea71a66d99b5be52b

---
+++ symforce/slam/imu_preintegration/imu_factor.h
@@ -0,0 +1,197 @@
+/* ----------------------------------------------------------------------------
+ * SymForce - Copyright 2022, Skydio, Inc.
+ * This source code is under the Apache 2.0 license found in the LICENSE file.
+ * ---------------------------------------------------------------------------- */
+
+#pragma once
+
+#include <vector>
+
+#include <Eigen/Core>
+
+#include <sym/pose3.h>
+#include <symforce/opt/factor.h>
+#include <symforce/opt/key.h>
+
+#include "./imu_preintegrator.h"
+#include "./preintegrated_imu_measurements.h"
+
+namespace sym {
+
+/**
+ * A factor for using on-manifold IMU preintegration in a SymForce optimization problem. By
+ * on-manifold, it is meant that the angular velocity measurements are composed as rotations
+ * rather than tangent-space approximations.
+ *
+ * Assumes IMU bias is constant over the preintegration window. Does not recompute the
+ * preintegrated measurements when the IMU bias estimate changes during optimization, but rather
+ * uses a first order approximation linearized at the IMU biases given during preintegration.
+ *
+ * The gravity argument should be [0, 0, -g] (where g is 9.8, assuming your world frame is
+ * gravity aligned so that the -z direction points towards the Earth) unless your IMU reads 0
+ * acceleration while stationary, in which case it should be [0, 0, 0].
+ *
+ * More generally, the gravity argument should yield the true acceleration when added to the
+ * accelerometer measurement (after the measurement has been converted to the world frame).
+ *
+ * Is a functor so as to store the preintegrated IMU measurements between two times.
+ * The residual computed is the local-coordinate difference between the final state (pose
+ * and velocity) and the state you would expect given the initial state and the IMU measurements.
+ *
+ * Based heavily on the on manifold ImuFactor found in GTSAM and on the paper:
+ *
+ * Christian Forster, Luca Carlone, Frank Dellaert, and Davide Scaramuzza,
+ * “IMU Preintegration on Manifold for Efficient Visual-Inertial Maximum-a-Posteriori Estimation”,
+ * Robotics: Science and Systems (RSS), 2015.
+ *
+ * Example Usage:
+ *
+ *   enum Var : char {
+ *     POSE = 'p',        // Pose3d
+ *     VELOCITY = 'v',    // Vector3d
+ *     ACCEL_BIAS = 'A',  // Vector3d
+ *     GYRO_BIAS = 'G',   // Vector3d
+ *     GRAVITY = 'g',     // Vector3d
+ *     EPSILON = 'e'      // Scalar
+ *   };
+ *
+ *   struct ImuMeasurement {
+ *     Eigen::Vector3d acceleration;
+ *     Eigen::Vector3d angular_velocity;
+ *     double duration;
+ *   };
+ *
+ *   int main() {
+ *     // Dummy function declarations
+ *     std::vector<ImuMeasurement> GetMeasurementsBetween(double start_time, double end_time);
+ *     std::vector<double> GetKeyFrameTimes();
+ *     Eigen::Vector3d GetAccelBiasEstimate(double time);
+ *     Eigen::Vector3d GetGyroBiasEstimate(double time);
+ *     void AppendOtherFactors(std::vector<sym::Factord> & factors);
+ *
+ *     // Example accelerometer and gyroscope covariances
+ *     const Eigen::Vector3d accel_cov = Eigen::Vector3d::Constant(1e-5);
+ *     const Eigen::Vector3d gyro_cov = Eigen::Vector3d::Constant(1e-5);
+ *
+ *     std::vector<sym::Factord> factors;
+ *
+ *     // GetKeyFrameTimes assumed to return at least 2 times
+ *     std::vector<double> key_frame_times = GetKeyFrameTimes();
+ *
+ *     // Integrating Imu measurements between keyframes, creating an ImuFactor for each
+ *     for (int i = 0; i < static_cast<int>(key_frame_times.size()) - 1; i++) {
+ *       const double start_time = key_frame_times[i];
+ *       const double end_time = key_frame_times[i + 1];
+ *
+ *       const std::vector<ImuMeasurement> measurements = GetMeasurementsBetween(start_time,
+ *                                                                               end_time);
+ *
+ *       // ImuPreintegrator should be initialized with the best guesses for the IMU biases
+ *       sym::ImuPreintegrator<double> integrator(GetAccelBiasEstimate(start_time),
+ *                                                GetGyroBiasEstimate(start_time));
+ *       for (const ImuMeasurement& meas : measurements) {
+ *         integrator.IntegrateMeasurement(meas.acceleration, meas.angular_velocity, accel_cov,
+ *                                         gyro_cov, meas.duration);
+ *       }
+ *
+ *       factors.push_back(sym::ImuFactor<double>(integrator)
+ *                             .Factor({{Var::POSE, i},
+ *                                      {Var::VELOCITY, i},
+ *                                      {Var::POSE, i + 1},
+ *                                      {Var::VELOCITY, i + 1},
+ *                                      {Var::ACCEL_BIAS, i},
+ *                                      {Var::GYRO_BIAS, i},
+ *                                      {Var::GRAVITY},
+ *                                      {Var::EPSILON}}));
+ *     }
+ *
+ *     // Adding any other factors there might be for the problem
+ *     AppendOtherFactors(factors);
+ *
+ *     sym::Optimizerd optimizer(sym::DefaultOptimizerParams(), factors);
+ *
+ *     // Build Values
+ *     sym::Valuesd values;
+ *     for (int i = 0; i < key_frame_times.size(); i++) {
+ *       values.Set({Var::POSE, i}, sym::Pose3d());
+ *       values.Set({Var::VELOCITY, i}, Eigen::Vector3d::Zero());
+ *     }
+ *     for (int i = 0; i < key_frame_times.size() - 1; i++) {
+ *       values.Set({Var::ACCEL_BIAS, i}, GetAccelBiasEstimate(key_frame_times[i]));
+ *       values.Set({Var::GYRO_BIAS, i}, GetGyroBiasEstimate(key_frame_times[i]));
+ *     }
+ *     // gravity should point towards the direction of acceleration
+ *     values.Set({Var::GRAVITY}, Eigen::Vector3d(0.0, 0.0, -9.8));
+ *     values.Set({Var::EPSILON}, sym::kDefaultEpsilond);
+ *     // Initialize any other items in values ...
+ *
+ *     optimizer.Optimize(&values);
+ *   }
+ */
+template <typename Scalar>
+class ImuFactor {
+ private:
+  PreintegratedImuMeasurements<Scalar> preintegrated_measurements_;
+  Eigen::Matrix<Scalar, 9, 9> sqrt_info_;
+
+ public:
+  /**
+   * Construct an ImuFactor connecting two states from the (preintegrated) imu measurements
+   * between them.
+   */
+  ImuFactor(const ImuPreintegrator<Scalar>& preintegrator);
+
+  /**
+   * Construct a Factor object that can be passed to an Optimizer object given the keys to
+   * be passed to the function.
+   *
+   * Convenience method to avoid manually specifying which arguments are optimized.
+   */
+  sym::Factor<Scalar> Factor(const std::vector<Key>& keys_to_func) const;
+
+  /**
+   * Calculate a between factor on the product manifold of the pose and velocity where the prior
+   * is calculated from the preintegrated IMU measurements.
+   *
+   * Time step i is the time of the first IMU measurement of the interval.
+   * Time step j is the time after the last IMU measurement of the interval.
+   *
+   * Args:
+   *     pose_i: Pose at time step i (world_T_body)
+   *     vel_i: Velocity at time step i (world frame)
+   *     pose_j: Pose at time step j (world_T_body)
+   *     vel_j: Velocity at time step j (world frame)
+   *     accel_bias_i: The bias of the accelerometer measurements between timesteps i and j
+   *     gyro_bias_i: The bias of the gyroscope measurements between timesteps i and j
+   *     gravity: Acceleration due to gravity (in the same frame as pose_x and vel_x),
+   *              i.e., the vector which when added to the accelerometer measurements
+   *              gives the true acceleration (up to bias and noise) of the IMU.
+   *     epsilon: epsilon used for numerical stability
+   *
+   * Outputs:
+   *     res: The 9dof whitened local coordinate difference between predicted and estimated state
+   *     jacobian: (9x24) jacobian of res wrt args pose_i (6), vel_i (3), pose_j (6), vel_j (3),
+   *               accel_bias_i (3), gyro_bias_i (3)
+   *     hessian: (24x24) Gauss-Newton hessian for args pose_i (6), vel_i (3), pose_j (6),
+   *              vel_j (3), accel_bias_i (3), gyro_bias_i (3)
+   *     rhs: (24x1) Gauss-Newton rhs for args pose_i (6), vel_i (3), pose_j (6), vel_j (3),
+   *          accel_bias_i (3), gyro_bias_i (3)
+   */
+  void operator()(const sym::Pose3<Scalar>& pose_i, const Eigen::Matrix<Scalar, 3, 1>& vel_i,
+                  const sym::Pose3<Scalar>& pose_j, const Eigen::Matrix<Scalar, 3, 1>& vel_j,
+                  const Eigen::Matrix<Scalar, 3, 1>& accel_bias_i,
+                  const Eigen::Matrix<Scalar, 3, 1>& gyro_bias_i,
+                  const Eigen::Matrix<Scalar, 3, 1>& gravity, const Scalar epsilon,
+                  Eigen::Matrix<Scalar, 9, 1>* const res = nullptr,
+                  Eigen::Matrix<Scalar, 9, 24>* const jacobian = nullptr,
+                  Eigen::Matrix<Scalar, 24, 24>* const hessian = nullptr,
+                  Eigen::Matrix<Scalar, 24, 1>* const rhs = nullptr) const;
+};
+
+using ImuFactord = ImuFactor<double>;
+using ImuFactorf = ImuFactor<float>;
+
+}  // namespace sym
+
+extern template class sym::ImuFactor<double>;
+extern template class sym::ImuFactor<float>;
