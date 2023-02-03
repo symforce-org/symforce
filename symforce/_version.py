597535925d4f5ae0d2534dfedb6ad4df36c7f7ff
Aaron <aaron@skydio.com>
2023-02-03 09:54:24 +0000

    [SymForce] Bump version to 0.8.0
    Reviewers: bradley,hayk,nathan,chao
    Topic: sf-v0.8
    GitOrigin-RevId: 0f45ae10d41360c5727edb17b4be4a48136aa2cf

--- symforce/_version.py
+++ symforce/_version.py
@@ -3,4 +3,4 @@
 # This source code is under the Apache 2.0 license found in the LICENSE file.
 # ----------------------------------------------------------------------------
 
-version = "0.7.0"
+version = "0.8.0"
