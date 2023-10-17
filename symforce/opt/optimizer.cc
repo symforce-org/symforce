dbe9b21aa1d89690ac9bc62ce54d0b04f64b53b8
Aaron Miller <78561124+aaron-skydio@users.noreply.github.com>
2023-10-17 02:21:01 -0700

    Add something that should warn

--- symforce/opt/optimizer.cc
+++ symforce/opt/optimizer.cc
@@ -20,6 +20,8 @@ sym::optimizer_params_t sym::DefaultOptimizerParams() {
   const double early_exit_min_reduction = 1e-6;
   const bool enable_bold_updates = false;
 
+  const bool something_unused;
+
   return sym::optimizer_params_t{
       verbose,
       initial_lambda,
