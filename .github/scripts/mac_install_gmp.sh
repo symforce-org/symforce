45392769651812d3d011fe6278b136250c7a17eb
Aaron Miller <78561124+aaron-skydio@users.noreply.github.com>
2023-11-01 22:58:29 +0000

    [SymForce-External] CI Updates
    - Fix wheel build on macos (credit to @Djolo1802)
    - Clang 15
    
    Topic: sfci-misc
    Closes symforce-org/symforce#372
    GitOrigin-RevId: db3cd01247bf0e4868b8c1d6dc7fe3b2c374a162

--- .github/scripts/mac_install_gmp.sh
+++ .github/scripts/mac_install_gmp.sh
@@ -10,8 +10,8 @@ set -ex
 if [ "$CIBW_ARCHS_MACOS" = "arm64" ]; then
   mkdir arm-homebrew
   curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C arm-homebrew
-  response=$(./arm-homebrew/bin/brew fetch --force --bottle-tag=arm64_big_sur gmp | grep "Downloaded to: " | cut -c 16-)
-  echo $response
-  # parsed=($response)
-  ./arm-homebrew/bin/brew install $response
+  cache_path=$(./arm-homebrew/bin/brew --cache gmp)
+  echo "Using cache_path: ${cache_path}"
+  curl --output ${cache_path} https://symforce.org/downloads/gmp-6.2.1_1.arm64_big_sur.bottle.tar.gz
+  ./arm-homebrew/bin/brew install ${cache_path}
 fi
