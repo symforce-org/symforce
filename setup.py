c8be4eb526096054262d98dc5e1901a1a4a09b45
Aaron <aaron@skydio.com>
2024-07-29 19:41:09 +0000

    [SymForce] Bump mypy, ruff
    Topic: sf-mypy-ruff
    Reviewers: ryan-b,brad,nathan
    GitOrigin-RevId: 58c1c5026656e31622b9664c359457b8caf4b2f2

--- setup.py
+++ setup.py
@@ -451,14 +451,14 @@ if __name__ == "__main__":
                 "argh",
                 "coverage",
                 "jinja2~=3.0",
-                "mypy~=1.9.0",
+                "mypy~=1.11.0",
                 "numba",
                 # 6.13 fixes pip >=23.1 support
                 "pip-tools>=6.13",
                 # NOTE(aaron): v1.0 changed lots of things, we'll need to update and regenerate
                 "pybind11-stubgen<1.0",
                 "pylint~=2.16",
-                "ruff~=0.4.4",
+                "ruff~=0.5.3",
                 "types-jinja2",
                 "types-requests",
                 "types-setuptools",
