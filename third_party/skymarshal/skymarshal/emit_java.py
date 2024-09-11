b896833d8d0711fbd340047c1f735d3e63906d56
Steve Moskovchenko <stevemo@skydio.com>
2024-09-11 00:46:54 +0000

    [Skymarshal] Java enum types
    Topic: lcmtype-java-enum-strings
    
    GitOrigin-RevId: 6c233a27881cdfba41d0b505229353b4977c367f

--- third_party/skymarshal/skymarshal/emit_java.py
+++ third_party/skymarshal/skymarshal/emit_java.py
@@ -396,7 +396,9 @@ class SkymarshalJava(SkymarshalLanguage):
             for enum in package.enum_definitions:
                 # TODO(jeff): generate a java enum class instead of an equivalent struct
                 javaclass = JavaClass(package, enum.equivalent_struct, args)
-                file_map[javaclass.fullpath] = render("lcmtype.java.template", lcmtype=javaclass)
+                file_map[javaclass.fullpath] = render("lcmtype.java.template", lcmtype=javaclass,
+                                                      enum_cases=enum.cases,
+                                                      enum_storage_type=TYPE_MAP[enum.storage_type_ref.name].constant)
 
         if args.java_srcjar is not None:
             zip_contents = BytesIO()
